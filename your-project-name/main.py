5401b7ede1acb420aed4ea4d4a66684fce217020
justcallmeeth <ethanparr10@gmail.com>
2025-07-28 16:32:42 +1000

    Working so far

--- your-project-name/main.py
+++ your-project-name/main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,12 +11,25 @@ def main():
     print("Screen height: 720")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    my_player = Player(x, y)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+
+        my_player.update(dt)
+
         screen.fill("black")
+        my_player.draw(screen)
         pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
