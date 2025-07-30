6d13b9dd2733f01f84e2b3794d41945863c1ea73
justcallmeeth <ethanparr10@gmail.com>
2025-07-30 20:44:07 +1000

    Chapter 3 of Boot.Dev Asteroids

--- your-project-name/main.py
+++ your-project-name/main.py
@@ -2,10 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable,)
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
@@ -17,6 +26,7 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     my_player = Player(x, y)
+    asteroid_field = AsteroidField()
     
     while True:
         for event in pygame.event.get():
@@ -24,10 +34,18 @@ def main():
                 return
         dt = clock.tick(60) / 1000
 
-        my_player.update(dt)
+        updateable.update(dt)
+
+        # Check for collisions between player and asteroids
+        for asteroid in asteroids:
+            if my_player.collides_with(asteroid):
+                print("Game over!")
+                sys.exit()
+        # Exit the program
 
         screen.fill("black")
-        my_player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         
 
